3c26afda1c425f8c5069fc68f4c0f325260f0f4e
simone-art <simoneperiodista3@gmail.com>
2020-02-21 23:42:37 -0300

    O cõdigo ainda não trás a imagem do pokemon api

--- js/pokemon.js
+++ js/pokemon.js
@@ -10,29 +10,28 @@ const pokemon = "https://pokeapi.co/api/v2/pokemon/"
 //FUNÇAO NO BOTÂO//
 botao.addEventListener('click', (e) => ConsultarPokemon());
 
-// let pokemonNumero = pokemon.id;
-// let name = pokemon.name;
-
 
 function ConsultarPokemon() {
     event.preventDefault();
     const id = input3.value;
     console.log(id);
-
+    
     fetch(`https://pokeapi.co/api/v2/pokemon/?q=${id}`)
-
+    
     .then(function (response) {
         return response.json();
-        })
+    })
     .then(function(pokemon) {
-           console.log(pokemon.results[id].name)
-           card.innerHTML = `
-    <h1>Name:${pokemon.results[id].name}</h1>
-    `
+        console.log(pokemon.results[id].name)
+        card.innerHTML = `
+        <img src="${pokemon.results[id].urlImagem}">
+            <h1>Name:${pokemon.results[id].name}</h1>
+            `
     })
     .catch(err => console.log(err));
 }
 
+// data.sprites.front_default
 
 
 
