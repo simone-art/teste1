191b4add7b2b75b09879276612bffcd9c5623815
simone-art <simoneperiodista3@gmail.com>
2020-02-21 21:31:02 -0300

    o código printa a Api no console

--- js/pokemon.js
+++ js/pokemon.js
@@ -2,57 +2,51 @@ const input = document.querySelector(".input-pokemon");
 const botao = document.querySelector(".btn-information");
 const container = document.getElementById("container-pokemon");
 const card = document.querySelector('#cards');
-// const url = 'https://pokeapi.co/api/v2/pokemon/80'
+const input3 = document.getElementById("pokemon-numero");
 
-const url = 'https://pokeapi.co/api/v2/pokemon/?q=${valorInput}'
+//API ENDPOINT//
+const pokemon = "https://pokeapi.co/api/v2/pokemon/"
+
+//FUNÇAO NO BOTÂO//
+botao.addEventListener('click', (e) => ConsultarPokemon());
+
+// let pokemonNumero = pokemon.id;
+// let name = pokemon.name;
 
 
-botao.addEventListener('click', (e) => fazerFetch(e));
+function ConsultarPokemon() {
+    event.preventDefault();
+    const id = input3.value;
+    console.log(id);
 
-function CarregarPokemon(id) {
-    // event.preventDefault();
-    // const id = input.value;
-    // console.log(id)
-    // const valorInput = input.value;
-    // console.log(valorInput);
-    // const url = 'https://pokeapi.co/api/v2/version/';
-    // const url = './data/data.json';
-    // const data = JSON.parse(data/data.json)
-    
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    
-    // fetch('https://pokeapi.co/api/v2/pokemon/80')
+    fetch(`https://pokeapi.co/api/v2/pokemon/?q=${id}`)
+
     .then(function (response) {
         return response.json();
         })
-        .then(function (pokemon) {
-            let card = document.getElementById('cards')
-            card.innerHTML = `
-            <p>${pokemon.name}</p>
-            `;
-            console.log(pokemon.name);
-        })
+    .then(function(pokemon) {
+           console.log(pokemon)
+    })
+    .catch(err => console.log(err));
+}
+
+function MostrarPokemon(){
+    card.innerHTML = `
+    <div id="cards">
+    <h1>Name:${pokemon.name}</h1>
+    </div>
+    <div id="info-pokemon">
+    <h2 class="number">Nº ${pokemon.id}</h2>
+    </div>`;
+    
+    return card
+}
         
-    }
     
-    CarregarPokemon(1)
-    
-    // <img src = '${data.sprites.front_default}'/>//
-// function showPokemon(jsonObj) {
-//     let pokemon = jsonObj['pokemon']
 
-//     for (var i = 0; i < pokemon.length; i++) {
-//     const h1 = document.createElement('h1')
-//     h1.textContent = name.value;
-//     const url = document.createElement('a')
-//     url.textContent = url.value;
-//     const position = document.createElement('p')
-//     position.textContent = position.value;
 
-//     container.appendChild(card);    
-//     card.appendChild(h1);
-//     card.appendChild(url);
-//     card.appendChild(position);
-//     }
-// }
+
+
+
+
 
