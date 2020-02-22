79a90efc9ebe207d91d3fa1dc84465bb411447ff
simone-art <simoneperiodista3@gmail.com>
2020-02-22 09:41:27 -0300

    Ao colocar no src a url do sprites front-default aparece a imagem do pokemon

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,11 +1,14 @@
 const input = document.querySelector(".input-pokemon");
 const botao = document.querySelector(".btn-information");
 const container = document.getElementById("container-pokemon");
-const card = document.querySelector('#cards');
+const card = document.querySelector('.cards');
 const input3 = document.getElementById("pokemon-numero");
+const lista = document.getElementById("listaPokemon")
 
 //API ENDPOINT//
 const pokemon = "https://pokeapi.co/api/v2/pokemon/"
+
+
 
 //FUNÇAO NO BOTÂO//
 botao.addEventListener('click', (e) => ConsultarPokemon());
@@ -16,22 +19,36 @@ function ConsultarPokemon() {
     const id = input3.value;
     console.log(id);
     
-    fetch(`https://pokeapi.co/api/v2/pokemon/?q=${id}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/?q=${id}.png`)
     
     .then(function (response) {
         return response.json();
     })
     .then(function(pokemon) {
         console.log(pokemon.results[id].name)
+        console.log(pokemon.results[id].url)
         card.innerHTML = `
-        <img src="${pokemon.results[id].urlImagem}">
-            <h1>Name:${pokemon.results[id].name}</h1>
-            `
+        <h1>Name:${pokemon.results[id].name}</h1>
+        <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">
+            
+        `
     })
-    .catch(err => console.log(err));
+    
 }
 
-// data.sprites.front_default
+// function MostrarPokemon(){
+//     let imagem = document.getElementsByTagName("img")[0]
+//     document.setAttribute("src", pokemon.results[id].sprites.front_default)
+    
+//     card.innerHTML = `
+//         <img src="${imagem}">
+//             <h1>Name:${pokemon.results[id].name}</h1>
+            
+//         `
+    
+//     .catch(err => console.log(err));
+// }
+
 
 
 
