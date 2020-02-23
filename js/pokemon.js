0d92511fe4d48358b9a7b6a70fa72b20fb6fa5ec
simone-art <simoneperiodista3@gmail.com>
2020-02-23 20:59:31 -0300

    feito código para criar 30 cards. Não funciona

--- js/pokemon.js
+++ js/pokemon.js
@@ -41,8 +41,7 @@ function ConsultarPokemon() {
         let valor = document.getElementById("opcoes").value;
         let texto = document.getElementById("opcoes");
         let selected = texto.options[texto.selectedIndex].text
-        // const id = selectedValue.value
-        console.log(selected)
+        // console.log(selected)
 
         fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=10`)
     
@@ -55,7 +54,12 @@ function ConsultarPokemon() {
         console.log(pokemon)
 
         if (selected == 10){
-                const html = `
+
+            let results = []
+            let names = results.map(function(){
+                return results.name
+            })
+            card.innerHTML = `
                 <h1>Name:${pokemon.results[0].name}</h1>
                 <h1>Name:${pokemon.results[1].name}</h1>
                 <h1>Name:${pokemon.results[2].name}</h1>
@@ -65,15 +69,42 @@ function ConsultarPokemon() {
                 <h1>Name:${pokemon.results[6].name}</h1>
                 <h1>Name:${pokemon.results[7].name}</h1>
                 <h1>Name:${pokemon.results[8].name}</h1>
-                <h1>Name:${pokemon.results[9].name}</h1>
+                // <h1>Name:${pokemon.results[9].name}</h1>
 
                 <h2> url: ${pokemon.results[0].url}</h2>
                 
                 `
-                const pokemonDiv = document.querySelector('.cards')
-                pokemonDiv.innerHTML = html;
+                
 
         }
         
     })
+}
+
+//Criar Cards//
+
+const cardNames = [];
+const cardUrl = [];
+
+function cardCreate (cardNames, cardUrl){
+    this.cardNames = cardNames
+    this.cardUrl = cardUrl
+}
+
+const createDesk = () => {
+let desk = []
+deck.length = 0;
+cardNames.forEach(_cardName => {
+cardUrl.forEach(_cardUrl => {
+    desk.push(newCard(_cardName, _cardUrl));
+    console.log(deck)
+    deck.forEach(_cardCreate => {
+        let eachCard = document.createElement('div');
+        eachCard.innerHTML = cardCreate.cardNames + "" + cardCreate.cardUrl;
+        eachCard.classList.add('cards')
+        document.getElementById("container-deck").appendChild(eachCard);
+    })
+})
+
+})
 }
