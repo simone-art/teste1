862cb85506f0eaf22f4f4f44fe8c3e6bbf2ac4bf
simone-art <simoneperiodista3@gmail.com>
2020-02-23 19:04:17 -0300

    Criado código para pegar dados da array results pokemon

--- js/pokemon.js
+++ js/pokemon.js
@@ -3,7 +3,8 @@ const botao = document.querySelector(".btn-information");
 const container = document.getElementById("container-pokemon");
 const card = document.querySelector('.cards');
 const input3 = document.getElementById("pokemon-numero");
-const lista = document.getElementById("listaPokemon")
+const selectedValue = document.getElementById("valorInput")
+
 
 //API ENDPOINT//
 const pokemon = "https://pokeapi.co/api/v2/pokemon/"
@@ -33,15 +34,39 @@ function ConsultarPokemon() {
         <h2> Tipo: "https://pokeapi.co/api/v2/type/${id}/"</h2>
         `
     })
+}
     
+    function MostrarSelectedPokemon(){
+        
+        let valor = document.getElementById("opcoes").value;
+        let texto = document.getElementById("opcoes");
+        let selected = texto.options[texto.selectedIndex].text
+        const id = selectedValue.value
+        console.log(selected)
+
+        fetch(`https://pokeapi.co/api/v2/pokemon?q=${id}offset=0&limit=10`)
+    
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function(pokemon) {
+        // console.log(pokemon.results[id].name)
+        // console.log(pokemon.results[id].url)
+
+        if (selected == 10){
+            let results = []
+            for (results = 0;  results.length < 10; results++) {
+                console.log(pokemon.results[1].name);
+              };
+            const html = `
+            <h1>Name:${pokemon.results[id].name}</h1>
+            <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon?q=${id}offset=0&limit=9.png">
+            <h2> url: ${pokemon.results[id].url}</h2>
+            
+            `
+            const pokemonDiv = document.querySelector('.cards')
+            pokemonDiv.innerHTML = html;
+        }
+        
+    })
 }
-
-function MostrarSelectedPokemon(){
-    let valor = document.getElementById("opcoes").value;
-    let texto = document.getElementById("opcoes");
-    let selected = texto.options[texto.selectedIndex].text
-    console.log(selected)
-}
-
-
-
