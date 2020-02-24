3091d638ad5db5d3cdc4e358e625db5964630359
simone-art <simoneperiodista3@gmail.com>
2020-02-24 12:46:53 -0300

    Usado map e printa todos os names e url dos 10 primeiros pokemon

--- js/pokemon.js
+++ js/pokemon.js
@@ -19,69 +19,79 @@ function ConsultarPokemon() {
     event.preventDefault();
     const id = input3.value;
     console.log(id);
-    
+
     fetch(`https://pokeapi.co/api/v2/pokemon/?q=${id}.png`)
-    
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(pokemon) {
-        console.log(pokemon.results[id].name)
-        console.log(pokemon.results[id].url)
-        card.innerHTML = `
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (pokemon) {
+            console.log(pokemon.results[id].name)
+            console.log(pokemon.results[id].url)
+            card.innerHTML = `
         <h1>Name:${pokemon.results[id].name}</h1>
         <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">
         <h2> Tipo: "https://pokeapi.co/api/v2/type/${id}/"</h2>
         `
-    })
-}
-    
-    function MostrarSelectedPokemon(){
-        
-        let valor = document.getElementById("opcoes").value;
-        let texto = document.getElementById("opcoes");
-        let selected = texto.options[texto.selectedIndex].text
-        // console.log(selected)
-
-        fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=10`)
-    
-        .then(function (response) {
-        return response.json();
         })
-        .then(function(pokemon) {
-        console.log(pokemon.results.name)
-        console.log(pokemon.results.url)
-        console.log(pokemon)
+}
 
-        if (selected == 10){
+function MostrarSelectedPokemon() {
 
-            // let results = []
-            // let names = results.map(function(){
-            //     return results.name
-            // })
+    let valor = document.getElementById("opcoes").value;
+    let texto = document.getElementById("opcoes");
+    let selected = texto.options[texto.selectedIndex].text
+    // const id = []
+    // let name = []
+    console.log(selected)
 
-            card1.innerHTML = `
-                <h1>Name:${pokemon.results[0].name}</h1>
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=10`)
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (pokemon) {
+            console.log(pokemon.results)
+            console.log(pokemon.results.url)
+            console.log(pokemon)
+
+            if (selected == 10) {
+
+                let pokemonResults = []
+                
+                function getName(item) {
+                    let fullname = [item.name].join("");
+                    return fullname;
+                }
+
+                function getUrl(item) {
+                    let fullurl = [item.url].join(" ");
+                    return fullurl;
+                }
+
+                card1.innerHTML = `
+                <h1>Name:${pokemon.results.map(getName)}</h1>
+                <h1>Url:${pokemon.results.map(getUrl)}</h1>
                 `
-            card2.innerHTML = `
-                <h1>Name:${pokemon.results[1].name}</h1>
+                card2.innerHTML = `
+                <h1>Name:${pokemon.results.map(getName)}</h1>
             `
-            card3.innerHTML = `
-            <h1>Name:${pokemon.results[2].name}</h1>
-            
-            `
-            card3.innerHTML = `
-            <h1>Name:${pokemon.results[3].name}</h1>
-            
-            `
-            card4.innerHTML = `
-            <h1>Name:${pokemon.results[4].name}</h1>
-            
-            `
-            card5.innerHTML = `
-            <h1>Name:${pokemon.results[5].name}</h1>
-            
-            `
+                // card3.innerHTML = `
+                // <h1>Name:${pokemon.results[2].name}</h1>
+
+                // `
+                // card3.innerHTML = `
+                // <h1>Name:${pokemon.results[3].name}</h1>
+
+                // `
+                // card4.innerHTML = `
+                // <h1>Name:${pokemon.results[4].name}</h1>
+
+                // `
+                // card5.innerHTML = `
+                // <h1>Name:${pokemon.results[5].name}</h1>
+
+                // `
                 // <h1>Name:${pokemon.results[3].name}</h1>
                 // <h1>Name:${pokemon.results[4].name}</h1>
                 // <h1>Name:${pokemon.results[5].name}</h1>
@@ -91,13 +101,13 @@ function ConsultarPokemon() {
                 // // <h1>Name:${pokemon.results[9].name}</h1>
 
                 // <h2> url: ${pokemon.results[0].url}</h2>
-                
-                // `
-                
 
-        }
-        
-    })
+                // `
+
+
+            }
+
+        })
 }
 
 //Criar Cards//
